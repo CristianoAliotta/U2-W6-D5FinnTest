09d7123e99abf1579fa269da35e7f87fbde55629
CristianoAliotta <cristiano.aliotta@outlook.com>
2022-10-07 19:40:21 +0200

    dark mode function implemented

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
-function active() {
-  document.getElementsByClassName("actual-tab").classList.add("active");
+let elements = document.querySelectorAll(".dark");
+function changeTheme(event) {
+  for (i = 0; i < elements.length; i++) {
+    if (event.target.checked) {
+      elements[i].classList.replace("light--theme", "dark--theme");
+    } else {
+      elements[i].classList.replace("dark--theme", "light--theme");
+    }
+  }
 }
