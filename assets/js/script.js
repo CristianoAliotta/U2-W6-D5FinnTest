06768e3fd29909775d80ed0aca2d1edaeef0e6b0
CristianoAliotta <cristiano.aliotta@outlook.com>
2022-10-08 15:42:03 +0200

    dark mode improved

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,20 @@
 let elements = document.querySelectorAll(".dark");
+let buttons = document.querySelectorAll(".btn-darkmode");
 function changeTheme(event) {
   for (i = 0; i < elements.length; i++) {
     if (event.target.checked) {
       elements[i].classList.replace("light--theme", "dark--theme");
     } else {
       elements[i].classList.replace("dark--theme", "light--theme");
+    }
+  }
+}
+function changeButton(event) {
+  for (i = 0; i < buttons.length; i++) {
+    if (event.target.checked) {
+      buttons[i].classList.replace("light--button", "dark--button");
+    } else {
+      buttons[i].classList.replace("dark--button", "light--button");
     }
   }
 }
